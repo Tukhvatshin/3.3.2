fe0d2d28ef94cf4fcbf5b7cd6284fdb8d105fcf9
Tukhvatshin <tukhvatshin@gmail.com>
2020-06-11 12:29:14 +0300

    1

---
+++ app/src/main/java/Utils.java
@@ -0,0 +1,40 @@
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.a332.R;
+
+public class Utils {
+
+    private static int sTheme;
+
+    public final static int THEME_DEFAULT = 0;
+    public final static int THEME_BLACK = 1;
+    public final static int THEME_GREEN = 2;
+    public final static int THEME_BLUE = 3;
+
+    public static void changeToTheme(Activity activity, int theme) {
+        sTheme = theme;
+        activity.finish();
+
+        activity.startActivity(new Intent(activity, activity.getClass()));
+
+    }
+
+    public static void onActivityCreateSetTheme(Activity activity) {
+        switch (sTheme) {
+            default:
+            case THEME_DEFAULT:
+                activity.setTheme(R.style.AppTheme);
+                break;
+            case THEME_BLACK:
+                activity.setTheme(R.style.ThemeBlack);
+                break;
+            case THEME_GREEN:
+                activity.setTheme(R.style.ThemeGreen);
+                break;
+            case THEME_BLUE:
+                activity.setTheme(R.style.ThemeBlue);
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/a332/MainActivity.java
+++ app/src/main/java/com/example/a332/MainActivity.java
@@ -1,31 +1,36 @@
 package com.example.a332;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    protected Spinner spinner;
-    protected Button button;
+    private Spinner spinner;
+    private Button button;
+    private Spinner spinnerTheme;
+    private Button buttonTheme;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Utils.onActivityCreateSetTheme(this);
         spinner = findViewById(R.id.spinner);
+        spinnerTheme = findViewById(R.id.spinnerTheme);
         button = findViewById(R.id.button);
+        buttonTheme = findViewById(R.id.buttonTheme);
         initSpinnerCountries();
+        initSpinnerStyleTheme();
         onClick();
+        onClickTheme();
     }
 
     private void initSpinnerCountries() {
@@ -41,22 +46,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int selected = spinner.getSelectedItemPosition();
                 switch (selected) {
-                    case 0:
+                    case 0: {
                         spinnerLocale("ru");
                         break;
-
-                    case 1:
-                        spinnerLocale("us");
-
-                        break;
-                    case 2:
+                    }
+                    case 1: {
                         spinnerLocale("de");
-
                         break;
+                    }
+                    case 2: {
+                        spinnerLocale("ang");
+                        break;
+                    }
                 }
             }
-        });
 
+        });
     }
 
     public void spinnerLocale(String language) {
@@ -64,7 +69,40 @@ public class MainActivity extends AppCompatActivity {
         Configuration config = new Configuration();
         config.setLocale(locale);
         getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+
     }
+
+
+    private void initSpinnerStyleTheme() {
+        ArrayAdapter<CharSequence> adapterTheme = ArrayAdapter.createFromResource(this,
+                R.array.styleTheme, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapterTheme);
+    }
+
+    private void onClickTheme() {
+        buttonTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedTheme = spinnerTheme.getSelectedItemPosition();
+                switch (selectedTheme) {
+                    case 0:
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_DEFAULT);
+                        break;
+                    case 1:
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLACK);
+                        break;
+                    case 2:
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
+                        break;
+                    case 3:
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                        break;
+                }
+                recreate();
+            }
+
+        });
+    }
+
 }
-
-
